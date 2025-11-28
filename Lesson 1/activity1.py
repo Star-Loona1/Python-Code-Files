b7978828eee32800cff74bb3e0252787f44e948f
Star-Loona1 <chiemelieezeife@gmail.com>
2025-11-28 20:51:01 +0300

    Add files via upload

---
+++ Lesson 1/activity1.py
@@ -0,0 +1,11 @@
+#A greeting
+print("Hello, world!")
+
+#My name
+print("My name is Glory")
+
+#A fun fact about me
+print("I love singing")
+
+#A simple calculation
+print("The result of 100 + 34 is ", 100 + 34)
