3ab5388997cc32c39340d04b2cd8b2e92a6fd9ee
Star-Loona1 <chiemelieezeife@gmail.com>
2025-12-02 22:00:30 +0300

    Add files via upload

---
+++ Lesson 2 after class project/activity.py
@@ -0,0 +1,11 @@
+#Let's store birthdays
+r = "Robin's birthday: 27th January, 2000"
+print(r)
+n = "Raven's birthday: 5th March, 2000"
+print(n)
+c = "Cyborg's birthday: 14th June, 1999"
+print(c)
+s = "Starfire's birthday: 30th August, 2005"
+print(s)
+b = "Beastboy's birthday: 20th November, 2003"
+print(b)
