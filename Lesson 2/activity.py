e103d068831883bb36a157f525a15be254bf4b85
Star-Loona1 <chiemelieezeife@gmail.com>
2025-12-02 20:39:29 +0300

    Add files via upload

---
+++ Lesson 2/activity.py
@@ -0,0 +1,14 @@
+#Print a sentence
+print("Hello world")
+
+#Print a number
+print(2)
+
+#Next Line after print
+print("Hey Python \n" )
+
+#Pass multiple arguments to print
+print("what is it", 4)
+
+#End the argument for printing
+print("Welcome to ",end= "*")
---
+++ Lesson 2/activity2.py
@@ -0,0 +1,11 @@
+#Creating different variables
+x = 5
+y = "Jose"
+print(x)
+print(y)
+
+codingal = "codingal"
+print(codingal)
+
+name = input("Enter your name: ")
+print("\n Hello", name, "\nwelcome to codingal")
