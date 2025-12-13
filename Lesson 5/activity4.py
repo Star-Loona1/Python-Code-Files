4918d34c23965b1d3d722248f1172c299b055c6a
Star-Loona1 <chiemelieezeife@gmail.com>
2025-12-13 14:43:00 +0300

    Add files via upload

---
+++ Lesson 5/activity4.py
@@ -0,0 +1,6 @@
+number = int(input('Enter any number: '))
+print("Number to be checked: ", number)
+if number%2==0:
+    print(number, "is an even number")
+else:
+    print(number, "is an odd number")
