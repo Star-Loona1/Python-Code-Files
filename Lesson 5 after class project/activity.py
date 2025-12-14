e505d4dbf55202c45f45338ec62594e922a5ce87
Star-Loona1 <chiemelieezeife@gmail.com>
2025-12-14 16:33:44 +0300

    Add files via upload

---
+++ Lesson 5 after class project/activity.py
@@ -0,0 +1,20 @@
+season = input('Enter the season: ')
+print('You have chosen ', season)
+if (season == 'winter'):
+    print("You should wear down jackets, boots, gloves, beanies, jackets or scarfs")
+    print('Dress code for winter')
+elif (season == 'summer'):
+    print("You should wear t-shirts, shorts, sundresses, skirts, light blouses, sandals, flipflops, light jackets or cardigans")
+    print('Dress code for summer')
+elif(season == 'spring'):
+    print("You should wear flowy blouses, crop tops, ruffled shirts in pastels or floral prints, jackets, raincoats, hats, umbrella, t-shirts, trousers or dresses")
+    print('Dress code for spring')
+elif(season == 'autumn'):
+    print("You should wear jackets, blazers, knitwear, tops, jeans, trousers, dresses, boots, shoes, scarves, hats or bags")
+    print('Dress code for autumn/fall')
+elif(season == 'rainy season'):
+    print("You should wear raincoats, tops, bottoms, sneakers, sandals/flipflops, waterproof boots, water resistant bags, bucket hats, umbrellas")
+    print('Dress code for rainy season')
+else:
+    print("You should wear perum tops, t-shirts, sleeveless blouses, lace up sweaters, boyfriend tee, cat eye sun glasses, fedora hats, straight jeans, maxi skirts, lace up oxford shoes, printed dresses or bucket bags")
+    print('Dress code for dry season')
