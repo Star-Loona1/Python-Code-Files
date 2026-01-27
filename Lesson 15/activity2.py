68c0811e4cbfde60c9b7e24284b66a435a9f967d
Star-Loona1 <chiemelieezeife@gmail.com>
2026-01-27 20:36:39 +0300

    Add files via upload

---
+++ Lesson 15/activity2.py
@@ -0,0 +1,13 @@
+"""Write a program to satisfy the following conditions of the given range: If the number is divisible by 20, it provides an output "twist." If the number is divisible by 15, it will pass (no output) If the number is divisible by 5, it will give an output “fizz.” If the number is divisible by 3, it will give an output "buzz." Otherwise, it will give the output of that number."""
+
+for j in range(10):
+    if j%20 == 0:
+        print("twist")
+    elif j%15 == 0:
+        pass
+    elif j%5 == 0:
+        print("fizz")
+    elif j%3 == 0:
+        print("buzz")
+    else:
+        print(j)
