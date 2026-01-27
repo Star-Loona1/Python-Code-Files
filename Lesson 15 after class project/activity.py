2892c382fac7d050f2e6152d5311753130f4c14d
Star-Loona1 <chiemelieezeife@gmail.com>
2026-01-27 21:15:08 +0300

    Add files via upload

---
+++ Lesson 15 after class project/activity.py
@@ -0,0 +1,6 @@
+bill = float(input("Enter your total bill amount:"))
+bill_paid = float(input("Enter the amount you paid:"))
+due_amount = bill - bill_paid
+print("Your bill is ", bill,"$")
+print("Your paid amount is ", bill_paid,"$")
+print("Your due amount is ", due_amount,"$")
