69ac8b0843cc770d9432813ac492cea95a85a01d
Star-Loona1 <chiemelieezeife@gmail.com>
2026-02-07 21:15:09 +0300

    Add files via upload

---
+++ Lesson 18 after class project/activity.py
@@ -0,0 +1,4 @@
+print('The months of the year')
+months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+for i in months:
+    print(i)
