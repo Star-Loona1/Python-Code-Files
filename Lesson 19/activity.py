c248b02c8ec73200e67cf072322e8c5ddb85fbbc
Star-Loona1 <chiemelieezeife@gmail.com>
2026-02-10 20:41:13 +0300

    Add files via upload

---
+++ Lesson 19/activity.py
@@ -0,0 +1,12 @@
+'''Write a program to perform the following operations on a List: 1. Create an empty list 2. A list with elements 3. Use * operator 4. Reverse a list'''
+
+list1 = []
+print(list1)
+list2 = ['Car', 'bag', 34, 5.9, 2006]
+print(list2)
+list3 = [6, 9, 2, 3, 10, 7]
+a = list3 * 4
+print(a)
+list4 = ['codingal', 'star', 2019, 'for']
+list4.reverse()
+print(list4)
