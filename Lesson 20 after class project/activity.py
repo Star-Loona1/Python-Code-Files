1b1002c72ef3bc8ac923b83e9d9cb5c00533755b
Star-Loona1 <chiemelieezeife@gmail.com>
2026-02-17 22:12:13 +0300

    Add files via upload

---
+++ Lesson 20 after class project/activity.py
@@ -0,0 +1,23 @@
+#tuple1
+tuple1 = (2,3,4,5,2)
+product = 1
+for item in tuple1:
+    product *= item
+    
+print('The product of the numbers is ', product)
+
+#tuple2
+tuple2 = (9, 8)
+product2 = 1
+for i in tuple2:
+    product2 *= i
+
+print('The product of the numbers is ', product2)
+
+#tuple3
+tuple3 = (9, 7, 3)
+product3 = 1
+for element in tuple3:
+    product3 *= element
+
+print('The product of the numbers is ', product3)
