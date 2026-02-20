b51b6c4b25e939ddf3e3d0d3b8c13e08d4ad64c8
Star-Loona1 <chiemelieezeife@gmail.com>
2026-02-20 20:45:14 +0300

    Add files via upload

---
+++ Lesson 22/activity2.py
@@ -0,0 +1,8 @@
+'''Write a program to find the intersection of two sets. Set1 = {green, blue} Set2 = {blue, yellow}'''
+set1 = {'green', 'blue'}
+print(set1)
+
+set2 = {'blue', 'yellow'}
+print(set2)
+
+print(set1.intersection(set2))
